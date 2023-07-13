710fb89a791de5e7daf9de3e5b18aebc24cdd03c
BarbarianTarkus <richvclaros@gmail.com>
2023-07-13 20:07:30 +0200

    Updates

---
+++ _app/immutable/chunks/config.0fabbd6f.js
@@ -0,0 +1,1 @@
+const t="R",o="Portfolio site of Ricardo Verduguez",s="Ricardo Verduguez",e=10,i=[{title:"About",route:"/about"},{title:"Blog",route:"/blog"}];export{s as a,o as b,i as n,e as p,t as s};
---
+++ _app/immutable/nodes/11.c0877063.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.d3757848.js";import{S as t,i as e}from"../chunks/index.9e48f9b1.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
