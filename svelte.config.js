cadeb89ff522562f1b28d547e1d4193c2a1794f1
BarbarianTarkus <richvclaros@gmail.com>
2023-07-13 23:47:36 +0200

    added vite github pages deployment, svelte docs configuration

--- svelte.config.js
+++ svelte.config.js
@@ -36,6 +36,14 @@ const config = {
 			strict: true
 		}),
 		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+                // ignore deliberate link to shiny 404 page
+                if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+                    return;
+                }
+
+                // otherwise fail the build
+                throw new Error(message);
 			entries: [
 				'*',
 				'/api/posts/page/*',
@@ -49,7 +57,7 @@ const config = {
 		},
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH,
-		}
+		}	
 	}
 };
 
